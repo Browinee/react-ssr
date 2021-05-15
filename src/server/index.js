f6db544b6a1e97295462dc85483eba69377de419
justin <>
2021-05-15 14:28:35 +0800

    feat: hydrate

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import { render } from './utils';
+import {getStore} from "../store";
+import {matchRoutes} from "react-router-config";
+import routes from '../Routes';
+import proxy from 'express-http-proxy';
+import config from "../../config";
 
 const app = express();
 app.use(express.static('public'));
-
+app.use('/api', proxy(`${config.baseUrl}`, {
+    proxyReqPathResolver: function (req) {
+        return '/ssr/api' + req.url;
+    }
+}));
 app.get('*', function (req, res) {
-  res.send(render(req));
+    const store = getStore();
+    const matchedRoutes = matchRoutes(routes, req.path);
+    const promises = [];
+    matchedRoutes.forEach(item => {
+      if (item.route.loadData) {
+        promises.push(item.route.loadData(store));
+      }
+    });
+    Promise.all(promises)
+        .then(() => {
+            res.send(render(store, routes, req));
+        })
+    res.send(render(store, routes, req));
 });
 
 var server = app.listen(3000);
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
 import {StaticRouter} from 'react-router-dom';
-import Routes from '../Routes';
 import {Provider} from "react-redux";
-import {getStore} from "../store";
+import {renderRoutes} from "react-router-config";
 
-export const render = (req) => {
+export const render = (store, routes, req) => {
+
     const content = renderToString((
-        <Provider store={getStore()}>
+        <Provider store={store}>
             <StaticRouter location={req.path} context={{}}>
-                {Routes}
+                {renderRoutes(routes)}
             </StaticRouter>
         </Provider>
     ))
@@ -20,6 +20,12 @@ export const render = (req) => {
 			</head>
 			<body>
 				<div id="root">${content}</div>
+				<script >
+				    window.context = {
+				      state: ${JSON.stringify(store.getState())}
+				    }
+				
+                </script>
 				<script src='/index.js'></script>
 			</body>
 		</html>
