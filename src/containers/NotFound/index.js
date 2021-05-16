eb10e9e5a142880e523161679b795c911d53c338
justin <>
2021-05-16 18:01:18 +0800

    feat: add NotFound

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 
 class NotFound extends Component {
 
-	componentDidMount() {
+	render() {
 		const { staticContext } = this.props;
 		staticContext && (staticContext.NOT_FOUND = true);
-	}
-
-	render() {
 		return <div>404, sorry, page not found</div>
 	}
 
